b8e1d47862b4334b3a6356759017cf9466e6b728
Tomgol1 <tomgol@gmail.com>
2025-08-15 23:46:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Flight search settings
 ORIGIN = "TLV"             # city or airport code
 DESTINATION = "KEF"        # city or airport code
-DEPARTURE_DATE = "2026-08-11"
-RETURN_DATE = "2026-08-12"
+DEPARTURE_DATE = "2026-01-11"
+RETURN_DATE = "2026-01-12"
 ALLOW_NEXT_DAY = True
 MAX_RESULTS = 10
