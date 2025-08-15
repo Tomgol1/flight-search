c3a572d4ac4f65cfa95170b1bcaeb5fab403700a
Tomgol1 <tomgol@gmail.com>
2025-08-15 23:26:50 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Flight search settings
+ORIGIN = "TLV"             # city or airport code
+DESTINATION = "LON"        # city or airport code
+DEPARTURE_DATE = "2025-09-01"
+RETURN_DATE = "2025-09-10"
+ALLOW_NEXT_DAY = False
+MAX_RESULTS = 5
