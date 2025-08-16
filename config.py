6751b93a1524275666f837a92ec7cc26c9677004
Tomgol1 <tomgol@gmail.com>
2025-08-16 15:43:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ RETURN_DATE = "2026-01-12"
 ALLOW_DEPARTURE_NEXT_DAY = False   # Search departure date +1 day
 ALLOW_RETURN_NEXT_DAY = False      # Search return date +1 day
 MAX_RESULTS = 10
+MAX_STOPS = 1              # Maximum number of stops (0=direct only, 1=max 1 stop, 2=max 2 stops, etc.)
