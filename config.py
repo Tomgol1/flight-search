af6de7d2a97624d7ed6c1cbaed3f79bce060277a
Tomgol1 <tomgol@gmail.com>
2025-08-17 11:28:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 # Flight search settings
 ORIGIN = "TLV"             # city or airport code
 DESTINATION = "KEF"        # city or airport code
-DEPARTURE_DATE = "2026-01-11"
-RETURN_DATE = "2026-01-12"
-ALLOW_DEPARTURE_NEXT_DAY = False   # Search departure date +1 day
-ALLOW_RETURN_NEXT_DAY = False      # Search return date +1 day
+DEPARTURE_DATE = "2026-08-11"
+RETURN_DATE = "2026-08-12"
+ALLOW_DEPARTURE_NEXT_DAY = True   # Search departure date +1 day
+ALLOW_RETURN_NEXT_DAY = True      # Search return date +1 day
 MAX_RESULTS = 10
-MAX_STOPS = 1              # Maximum number of stops (0=direct only, 1=max 1 stop, 2=max 2 stops, etc.)
+MAX_STOPS = 2              # Maximum number of stops (0=direct only, 1=max 1 stop, 2=max 2 stops, etc.)
